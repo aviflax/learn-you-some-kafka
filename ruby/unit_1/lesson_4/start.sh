c5fd56d890b2738fb3dbcb6ff2d5d00ee7387042
Diogo Scudelletti <diogo@scudelletti.com>
2018-05-02 17:48:56 +0200

    Fix network name on start script, network name is based on folder name

--- ruby/unit_1/lesson_4/start.sh
+++ ruby/unit_1/lesson_4/start.sh
@@ -5,7 +5,7 @@ set -ex
 cp ../../lesson.rb ../Dockerfile ../Gemfile ./
 
 ID=`docker build --quiet .`
-docker run -it --rm --network lesson1_default $ID
+docker run -it --rm --network lesson_1_default $ID
 
 # We’ll again leave the network up — i.e. not run
 # `docker-compose down` — because some more of the
