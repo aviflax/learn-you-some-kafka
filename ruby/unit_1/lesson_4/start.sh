d076297fb05b26d651246510c0c6603eca89d8b9
Diogo Scudelletti <diogo@scudelletti.com>
2018-05-08 09:18:55 +0200

    Rename network to unit-1 as suggested on PR

--- ruby/unit_1/lesson_4/start.sh
+++ ruby/unit_1/lesson_4/start.sh
@@ -5,7 +5,7 @@ set -ex
 cp ../../lesson.rb ../Dockerfile ../Gemfile ./
 
 ID=`docker build --quiet .`
-docker run -it --rm --network lesson-network $ID
+docker run -it --rm --network unit-1 $ID
 
 # We’ll again leave the network up — i.e. not run
 # `docker-compose down` — because some more of the
