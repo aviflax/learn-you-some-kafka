729a05a3e48a3e4af7d3a060ed59e4832a76933a
Avi Flax <avi.flax@fundingcircle.com>
2018-08-13 15:31:25 -0400

    Break up a too-long fragment

---
+++ ruby/jruby/unit_1/lesson_3/fragment_6.rb
@@ -0,0 +1,26 @@
+# ## Extra Credit
+#
+# Try running the lesson again. Observe what happens, and see if it lines up
+# with what you expect. If not, you should be able to find an explanation in
+# these docs on Consumer Groups:
+#   https://kafka.apache.org/documentation/#intro_consumers
+#
+#
+# ## Links and Further Reading
+#
+# * An introduction to consumer groups:
+#     https://kafka.apache.org/documentation/#intro_consumers
+#
+# * For more on how to _think_ about Kafka — i.e. as a messaging system, storage
+# system, data transport system, etc — and its data model, i.e. records vs
+# messages, the introduction in the Kafka docs is excellent:
+#     https://kafka.apache.org/documentation/#introduction
+#
+#   The entire introduction is excellent; these specific parts are particularly
+#   recommended:
+#
+#   * The introduction to the introduction
+#   * Kafka as a Messaging System
+#   * Kafka as a Storage System
+#   * Kafka for Stream Processing
+#   * Putting the Pieces Together
