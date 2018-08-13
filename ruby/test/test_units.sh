eb4ac4bca123e8133ea9ac37169fe02a87bdc493
Avi Flax <avi.flax@fundingcircle.com>
2018-08-13 12:54:20 -0400

    Enable testing of a specific unit or set of units

--- ruby/test/test_units.sh
+++ ruby/test/test_units.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo 'You must specify at least one path to test.'
+    exit 1
+fi
+
 set -ex
 
-for UNIT_PATH in ../*ruby/unit*
-do
+for UNIT_PATH; do
+  if [ ! -d "$UNIT_PATH/lesson_1" ]; then
+    echo "$UNIT_PATH does not appear to be a unit."
+    exit 1
+  fi
+
   # Run in a subshell so each iteration starts in the same dir
   (
   set -ex # Maybe this is inherited by the subshell?
