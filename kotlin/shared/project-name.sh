402dd4843fb575a61e0272e55b146e61ab886e6b
Avi Flax <avi.flax@fundingcircle.com>
2018-08-14 17:21:38 -0400

    WIP Kotlin

---
+++ kotlin/shared/project-name.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Determine docker-compose project name.
+#
+# This is meant to be run from within one of the start.sh or shutdown.sh scripts
+# from within a unit dir.
+#
+# We need this because otherwise units of different programming systems would
+# interfere with each other. i.e. the compose networks defined in
+# ruby/cruby/unit_1/docker-compose.yml and ruby/jruby/unit_1/docker-compose.yml
+# would by default use the project name `unit_1` which means that containers
+# started within those networks such as zookeeper or kafka could step on each
+# other; if containers are started within unit 1 of one system and not removed,
+# and then a lesson from unit 1 of a different system is started, it’ll see that
+# those containers exist and just use them. This could lead to unexpected,
+# puzzling, and hard-to-debug problems with the lessons.
+
+RUNTIME=$(cd .. && echo ${PWD##*/})
+UNIT=${PWD##*/}
+PROJECT="${RUNTIME}_${UNIT}"
+
+echo $PROJECT
---
+++ kotlin/unit_1/lesson_1/fragment_5.kts.rb
@@ -0,0 +1,25 @@
+# And that’s it for this lesson!
+#
+#
+# ## Quick Review
+#
+# * We’ve produced 3 records to Kafka, and confirmed that they actually made it
+# * Each record has a nil key and a string value
+# * Those string values are paragraphs of text without newlines
+#
+#
+# ## Next Lesson
+#
+# Up next in lesson 2: learn how to consume those records we just produced.
+#
+# To start the next lesson, run this in your shell:
+#
+# ./start.sh lesson_2
+#
+# If you won’t be running the next lesson right now, we recommend you shut down
+# the Kafka broker and ZooKeeper node that are running as Docker containers by
+# running ./shutdown.sh
+#
+# Note: If you do so, then when you do want to run the next lesson you’ll need
+# to start the unit over from lesson 1, as each lesson depends on the records
+# produced in the preceding lessons.
