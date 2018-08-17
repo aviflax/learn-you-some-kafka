21ef1f139cbbbeb29e2fff96b42540329e991d86
Avi Flax <avi.flax@fundingcircle.com>
2018-08-17 10:47:14 -0400

    Adapt test runner to simpler infrastructure

--- ruby/test/test_units.sh
+++ ruby/test/test_units.sh
@@ -20,26 +20,15 @@ for UNIT_PATH; do
   cp test_unit.rb "$UNIT_PATH/"
   cd $UNIT_PATH
 
-  # Copy Docker configs to unit level so the entire unit becomes the context.
-  # We also need files like Gemfile/Jarfile so the images build successfully.
-  cp shared/* ./
-
-  # Technique for getting “bare” name of current dir from here:
-  #   https://stackoverflow.com/a/1371283/7012 (it might prove problematic as it
-  # might require bash (which doesn’t ship with Alpine).)
-  RUNTIME=$(cd .. && echo ${PWD##*/})
-  UNIT=${PWD##*/}
-  PROJECT="${RUNTIME}_${UNIT}"
-
   # Start ZooKeeper and then the Kafka broker
-  docker-compose -p $PROJECT up -d zookeeper
+  docker-compose up -d zookeeper
   sleep 1 # Wait for ZooKeeper to come up before starting the Kafka broker
-  docker-compose -p $PROJECT up -d kafka
+  docker-compose up -d kafka
   sleep 3 # Wait for the Kafka broker to come up before proceeding with the test
 
-  docker-compose -p $PROJECT build
-  docker-compose -p $PROJECT run --rm --entrypoint "ruby test_unit.rb" unit
-  docker-compose -p $PROJECT down
+  docker-compose build
+  docker-compose run --rm unit ruby test_unit.rb
+  docker-compose down
   )
 done
 
